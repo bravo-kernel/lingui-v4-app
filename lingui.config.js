5af6915e20f417c5e32965b54daba8ca5a7c7908
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-04-22 13:09:48 +0200

    Add lingui config

---
+++ lingui.config.js
@@ -0,0 +1,26 @@
+const { formatter } = require("@lingui/format-po")
+
+const locales = ["en-us", "nl-nl"]
+
+if (process.env.NODE_ENV !== "production") {
+  locales.push("pseudo")
+}
+
+module.exports = {
+  locales: locales,
+  sourceLocale: "en-us",
+  pseudoLocale: "pseudo",
+  catalogs: [
+    {
+      path: "src/translations/locales/{locale}",
+      include: [
+        // "src/components",
+        // "src/core",
+        // "src/lib",
+        "src/pages",
+        // "src/translations/languages.ts",
+      ],
+    },
+  ],
+  format: formatter({ origins: false }),
+}
