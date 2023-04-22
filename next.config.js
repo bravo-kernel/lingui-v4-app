0d3e0bb2c59c17db76fd6528cdf0843f52901aeb
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-04-22 13:09:59 +0200

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
+const linguiConfig = require("./lingui.config")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    swcPlugins: [
+      [
+        "@lingui/swc-plugin", {}
+      ],
+    ],
+  },
+  i18n: {
+    locales: linguiConfig.locales,
+    defaultLocale: linguiConfig.sourceLocale,
+  },
 }
 
 module.exports = nextConfig
