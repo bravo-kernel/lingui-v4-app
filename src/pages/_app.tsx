e5c5b419640b4a36611d0a54344a958f2163adaa
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-04-22 17:31:39 +0200

    Enable Lingui for the application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { I18nProvider } from "@lingui/react";
+import { useLinguiInit } from "src/translations/utils";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const initializedI18n = useLinguiInit(pageProps.i18n);
+
+  return (
+    <I18nProvider i18n={initializedI18n}>
+      <Component {...pageProps} />
+    </I18nProvider>
+  );
 }
