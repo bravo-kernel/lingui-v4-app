b7f0efcfe6ed1466b89a6ce93d61e63166a92d1b
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-04-26 19:02:02 +0200

    Fix typescript error for utils useEffect()

--- src/translations/utils.ts
+++ src/translations/utils.ts
@@ -15,8 +15,6 @@ export function useLinguiInit(messages: Messages) {
 
   if (!isClient && locale !== i18n.locale) {
     // there is single instance of i18n on the server
-    // note: on the server, we could have an instance of i18n per supported locale
-    // to avoid calling loadAndActivate for (worst case) each request, but right now that's what we do
     i18n.loadAndActivate({ locale, messages });
   }
   if (isClient && i18n.locale === undefined) {
@@ -29,7 +27,7 @@ export function useLinguiInit(messages: Messages) {
     if (localeDidChange) {
       i18n.loadAndActivate({ locale, messages });
     }
-  }, [locale]);
+  }, [locale, messages]);
 
   return i18n;
 }
