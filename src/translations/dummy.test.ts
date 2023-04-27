88e93b3888eb26b77cdcd32e2b506a0934414a25
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-04-27 14:33:49 +0200

    Add the test suite integration with lingui swc-plugin

---
+++ src/translations/dummy.test.ts
@@ -0,0 +1,5 @@
+import { it, expect } from "vitest"
+
+it("is just a dummy test to make sure vitest works with lingui swc-plugin", () => {
+    expect("a").toEqual("a")
+})
---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { loadEnvConfig } from "@next/env"
+import { defineConfig } from "vitest/config"
+
+import react from "@vitejs/plugin-react-swc"
+import tsconfigPaths from "vite-tsconfig-paths"
+
+const projectDir = process.cwd()
+loadEnvConfig(projectDir)
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react({ plugins: [["@lingui/swc-plugin", {}]] }), tsconfigPaths()],
+  test: {
+    dir: "./",
+  },
+})
